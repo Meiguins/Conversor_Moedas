756496a3de78fa7537db6f3c3e14633f2c671eaa
meiguins <kauanmeiguins3@gmail.com>
2024-10-15 17:25:01 -0300

    alteracooes

--- src/Busca.java
+++ src/Busca.java
@@ -1,19 +1,22 @@
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Map;
 import java.util.Scanner;
 
-public class Busca{
-    public static void main(String[] args) throws IOException, InterruptedException{
+public class Busca {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Digite o valor que deseja converter");
         Double valorDigitado = sc.nextDouble();
+        sc.nextLine(); // Consumir a nova linha pendente
 
         System.out.println("Digite para qual moeda deseja converter: ");
-        var moeda = sc.nextLine();
+        String moeda = sc.nextLine().toUpperCase();
 
         String endereco = "https://v6.exchangerate-api.com/v6/93372b04d48a3b502e00f6e4/latest/USD";
 
@@ -21,9 +24,25 @@ public class Busca{
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endereco))
                 .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Converter a resposta JSON em objeto Java
+        Gson gson = new Gson();
+        ExchangeRatesResponse ratesResponse = gson.fromJson(response.body(), ExchangeRatesResponse.class);
+
+        // Verificar se a moeda solicitada está presente na resposta
+        if (ratesResponse.conversion_rates.containsKey(moeda)) {
+            Double taxa = ratesResponse.conversion_rates.get(moeda);
+            Double valorConvertido = valorDigitado * taxa;
+            System.out.printf("O valor %.2f USD convertido para %s é: %.2f%n", valorDigitado, moeda, valorConvertido);
+        } else {
+            System.out.println("Moeda não encontrada.");
+        }
     }
+}
+
+class ExchangeRatesResponse {
+    String base_code;
+    Map<String, Double> conversion_rates;
 }
